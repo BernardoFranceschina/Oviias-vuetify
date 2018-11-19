388559be9116e6fe951786ce2293eb9a69c51a8b
Bernardo <Bernardofranceschina@gmail.com>
2018-11-19 17:21:33 -0200

    Added navigation drawer

--- src/components/addProd.vue
+++ src/components/addProd.vue
@@ -1,6 +1,48 @@
 <template>
 
     <v-app>
+
+        <v-navigation-drawer app v-model="drawer">
+			<v-toolbar flat>
+				<v-list>
+					<v-list-tile>
+						<v-avatar>
+							<v-icon v-if="!isLogged">mdi-spin mdi-account-circle</v-icon>
+							<v-img v-else dark :src="user.photoURL"></v-img>
+						</v-avatar>
+						<v-list-tile-title class="title">
+							<span>⠀{{user ? user.displayName:"anom"}}</span>
+						</v-list-tile-title>
+					</v-list-tile>
+				</v-list>
+			</v-toolbar>
+			<v-divider></v-divider>
+			
+			<v-list two-line subheader>
+				<v-subheader>Nossa loja:</v-subheader>
+				<v-list-tile avatar>
+					<router-link class="white--text" to="/mostruario">
+					<v-list-tile-content>
+						<v-list-tile-title>Mostruário</v-list-tile-title>
+						<v-list-tile-sub-title>Confira nosso catálogo de produtos</v-list-tile-sub-title>
+					</v-list-tile-content>
+					</router-link>
+				</v-list-tile>
+				<v-list-tile avatar>
+					<router-link class="white--text" to="/carinho">
+					<v-list-tile-content>
+						<v-list-tile-title>Carinho</v-list-tile-title>
+						<v-list-tile-sub-title>Veja como está suas compras</v-list-tile-sub-title>
+					</v-list-tile-content>
+					</router-link>
+				</v-list-tile>
+			</v-list>
+		</v-navigation-drawer>
+	
+		<v-toolbar app>
+			<v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+			<v-toolbar-title>Oviia's Departament Store</v-toolbar-title>
+
         <v-container grid-list-xs>
 
             <v-form ref="input_form" v-model="form">  
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,9 +1,47 @@
 <template>
 	<v-app dark>
-		<v-toolbar>
+	
+		<v-navigation-drawer app v-model="drawer">
+			<v-toolbar flat>
+				<v-list>
+					<v-list-tile>
+						<v-avatar>
+							<v-icon v-if="!isLogged">mdi-spin mdi-account-circle</v-icon>
+							<v-img v-else dark :src="user.photoURL"></v-img>
+						</v-avatar>
+						<v-list-tile-title class="title">
+							<span>⠀{{user ? user.displayName:"anom"}}</span>
+						</v-list-tile-title>
+					</v-list-tile>
+				</v-list>
+			</v-toolbar>
+			<v-divider></v-divider>
+			
+			<v-list two-line subheader>
+				<v-subheader>Nossa loja:</v-subheader>
+				<v-list-tile avatar>
+					<router-link class="white--text" to="/mostruario">
+					<v-list-tile-content>
+						<v-list-tile-title>Mostruário</v-list-tile-title>
+						<v-list-tile-sub-title>Confira nosso catálogo de produtos</v-list-tile-sub-title>
+					</v-list-tile-content>
+					</router-link>
+				</v-list-tile>
+				<v-list-tile avatar>
+					<router-link class="white--text" to="/carinho">
+					<v-list-tile-content>
+						<v-list-tile-title>Carinho</v-list-tile-title>
+						<v-list-tile-sub-title>Veja como está suas compras</v-list-tile-sub-title>
+					</v-list-tile-content>
+					</router-link>
+				</v-list-tile>
+			</v-list>
+		</v-navigation-drawer>
+	
+		<v-toolbar app>
+			<v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
 			<v-toolbar-title>Oviia's Departament Store</v-toolbar-title>
 			<v-spacer></v-spacer>
-			<span>{{user ? user.displayName:"anom"}}</span>
 			<v-toolbar-items>
 				<v-btn icon>
 					<router-link to="/login" class="white--text">
@@ -13,16 +51,20 @@
 			</v-toolbar-items>
 		</v-toolbar>
 	
-		<v-parallax height="597" src="public/img2.jpg">
-			<v-layout align-center column justify-center>
-				<h2 class="display-3 --text text-xs-center mt-5">Bem-Vinda</h2>
-				<h2 class="display-2 --text text-xs-center">a Oviia's Departament Store</h2>
-				<v-btn>
-					<router-link to="/mostruario" class="white--text">Veja nosso mostruário</router-link>
-				</v-btn>
-			</v-layout>
-		</v-parallax>
-
+		<v-layout>
+			<v-flex xs12>
+				<v-parallax height="661" src="public/img2.jpg">
+					<v-layout align-center column justify-center>
+						<h2 class="display-3 --text text-xs-center mt-5">Bem-Vinda</h2>
+						<h2 class="display-2 --text text-xs-center">a Oviia's Departament Store</h2>
+						<v-btn>
+							<router-link to="/mostruario" class="white--text">Veja nosso mostruário</router-link>
+						</v-btn>
+					</v-layout>
+				</v-parallax>
+			</v-flex>
+		</v-layout>
+	
 		<v-footer class="pa-3">
 			<v-spacer></v-spacer>
 			<div>&copy; {{ new Date().getFullYear() }}</div>
@@ -35,19 +77,21 @@
 	export default {
 		data() {
 			return {
+				drawer: false,
+				isLogged: false,
 				user: {
-					"displayName": "anom"
+					"displayName": "anom",
 				},
 			}
 		},
-
 		created() {
 			firebase.auth().onAuthStateChanged(user => {
 				this.user = user;
+				if (this.user) this.isLogged = true;
 			})
 		},
-
-
+	
+	
 	}
 </script>
 
